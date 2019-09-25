fe28037cebf08c91932f3170a33676d9334bffdc
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-25 22:38:14 +0200

    Create utils.jl

---
+++ utils.jl
@@ -0,0 +1,58 @@
+#
+# https://github.com/eriklindernoren/ML-From-Scratch
+#
+
+import Combinatorics: with_replacement_combinations
+using Random
+
+""" Random shuffle of the samples in X and y """
+function shuffle_data(X, y, seed=nothing):
+    if seed:
+        np.random.seed(seed)
+    idx = collect(0:X.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
+end
+
+
+def normalize(X, axis=-1, order=2):
+    """ Normalize the dataset X """
+    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
+    l2[l2 == 0] = 1
+    return X / np.expand_dims(l2, axis)
+
+def polynomial_features(X, degree):
+    n_samples, n_features = np.shape(X)
+
+    def index_combinations():
+        combs = [combinations_with_replacement(range(n_features), i) for i in range(0, degree + 1)]
+        flat_combs = [item for sublist in combs for item in sublist]
+        return flat_combs
+    
+    combinations = index_combinations()
+    n_output_features = len(combinations)
+    X_new = np.empty((n_samples, n_output_features))
+    
+    for i, index_combs in enumerate(combinations):  
+        X_new[:, i] = np.prod(X[:, index_combs], axis=1)
+
+    return X_new
+
+
+def mean_squared_error(y_true, y_pred):
+    """ Returns the mean squared error between y_true and y_pred """
+    mse = np.mean(np.power(y_true - y_pred, 2))
+    return mse
+
+
+def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
+    """ Split the data into train and test sets """
+    if shuffle:
+        X, y = shuffle_data(X, y, seed)
+    # Split the training data from test data in the ratio specified in
+    # test_size
+    split_i = len(y) - int(len(y) // (1 / test_size))
+    X_train, X_test = X[:split_i], X[split_i:]
+    y_train, y_test = y[:split_i], y[split_i:]
+
+    return X_train, X_test, y_train, y_test
