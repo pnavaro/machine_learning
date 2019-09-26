9d8e1e2465e8463d90e5b74be54b3af938cb7bc5
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-09-26 17:30:52 +0200

    Update utils.jl

--- utils.jl
+++ utils.jl
@@ -3,56 +3,68 @@
 #
 
 import Combinatorics: with_replacement_combinations
-using Random
+using Random, LinearAlgebra
 
 """ Random shuffle of the samples in X and y """
-function shuffle_data(X, y, seed=nothing):
-    if seed:
-        np.random.seed(seed)
-    idx = collect(0:X.shape[0])
-    np.random.shuffle(idx)
-    return X[idx], y[idx]
+function shuffle_data(X, y, seed)
+    rng = MersenneTwister(seed)
+    idx = randperm(rng, size(X)[1])
+    return X[idx,:], y[idx]
+end
+
+""" Random shuffle of the samples in X and y """
+function shuffle_data(X, y)
+    idx = randperm(size(X)[1])
+    return X[idx,:], y[idx]
+end
+
+ """ Normalize the dataset X """
+function normalize(X)
+    l2 = sqrt.(sum(X.^2, dims=1))
+    l2 .+= l2 .== 0
+    return X / np.expand_dims(l2, axis)
+end
+
+function polynomial_features(X, degree)
+
+    n_samples, n_features = size(X)
+
+    function index_combinations()
+        combs = [with_replacement_combinations(1:n_features, i) for i in 0:degree+1]
+        flat_combs = [item for sublist in combs for item in sublist]
+        return flat_combs
+    end
+    
+    combinations = index_combinations()
+    n_output_features = length(combinations)
+    X_new = zeros(eltype(X), (n_samples, n_output_features))
+    
+    for (i, index_combs) in enumerate(combinations)
+        X_new[:, i] = prod(X[:, index_combs], dims=2)
+    end
+
+    return X_new
 end
 
 
-def normalize(X, axis=-1, order=2):
-    """ Normalize the dataset X """
-    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
-    l2[l2 == 0] = 1
-    return X / np.expand_dims(l2, axis)
-
-def polynomial_features(X, degree):
-    n_samples, n_features = np.shape(X)
-
-    def index_combinations():
-        combs = [combinations_with_replacement(range(n_features), i) for i in range(0, degree + 1)]
-        flat_combs = [item for sublist in combs for item in sublist]
-        return flat_combs
-    
-    combinations = index_combinations()
-    n_output_features = len(combinations)
-    X_new = np.empty((n_samples, n_output_features))
-    
-    for i, index_combs in enumerate(combinations):  
-        X_new[:, i] = np.prod(X[:, index_combs], axis=1)
-
-    return X_new
-
-
-def mean_squared_error(y_true, y_pred):
-    """ Returns the mean squared error between y_true and y_pred """
-    mse = np.mean(np.power(y_true - y_pred, 2))
+""" Returns the mean squared error between y_true and y_pred """
+function mean_squared_error(y_true, y_pred)
+    mse = mean((y_true .- y_pred) .^ 2)
     return mse
+end
 
 
-def train_test_split(X, y, test_size=0.5, shuffle=True, seed=None):
-    """ Split the data into train and test sets """
-    if shuffle:
+""" Split the data into train and test sets """
+function train_test_split(X :: Array{Float64,2}, y :: Vector{Float64}; 
+                          test_size=0.5, shuffle=true, seed=nothing)
+    if shuffle
         X, y = shuffle_data(X, y, seed)
+    end
     # Split the training data from test data in the ratio specified in
     # test_size
-    split_i = len(y) - int(len(y) // (1 / test_size))
-    X_train, X_test = X[:split_i], X[split_i:]
-    y_train, y_test = y[:split_i], y[split_i:]
+    split_i = length(y) - (length(y) ÷ trunc(Int64,1 / test_size)) :: Int64
+    X_train, X_test = X[1:split_i,:], X[split_i:end,:]
+    y_train, y_test = y[1:split_i], y[split_i:end]
 
     return X_train, X_test, y_train, y_test
+end
